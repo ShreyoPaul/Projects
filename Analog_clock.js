7bb2aedf16b611b66ddc29813024c8be61dd576b
Shreyo Paul <90982938+ShreyoPaul@users.noreply.github.com>
2022-12-08 11:14:14 +0530

    Update Analog_clock.js

--- Analog_clock.js
+++ Analog_clock.js
@@ -22,12 +22,14 @@ dark.addEventListener("click", () => {
         document.body.style.backgroundColor = '#E6DDC4';
         dark.style.border = '1px solid #E6DDC4';
         clock.style.border = '1px solid #E6DDC4';
+        mn.style.backgroundColor = '#090c18';
         flag = false;
     }
     else{
         document.body.style.backgroundColor = '#090c18';
         dark.style.border = '1px solid #090c18';
         clock.style.border = '1px solid #090c18';
+        mn.style.backgroundColor = '#e6e6e6';
         flag = true;
     }
 })
